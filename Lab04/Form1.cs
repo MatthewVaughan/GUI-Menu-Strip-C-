2be7350d9a56f050d44849bd2f4ef8cecc680a82
MatthewVaughan <mattman94@gmail.com>
2016-05-08 09:44:40 -0600

    original upload

---
+++ Lab04/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        // The exitToolStripMenuItem1 method
+        // Purpose: To close the window and terminate the application
+        // Parameters: The object generating the event 
+        // and the event arguments
+        // Returns: None
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Matthew Vaughan\nCS1400\nLab #4");
+        }
+    }
+}
